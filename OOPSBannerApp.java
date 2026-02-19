53c8735bc400b44249abc51a2a590b4c6bd8b9f6
Om Pratap Dhaker <OPDhaker2007@gmail.com>
2026-02-19 22:07:34 +0530

    complete UC03

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,10 +1,15 @@
 public class OOPSBannerApp {
   public static void main(String[] args) {
-    System.out.println(String.join(" ", "  .***.  ", "  .***.  ", "*******. ", " .*****. "));
-    System.out.println(String.join(" ", " *     * ", " *     * ", "**     **", "**     **"));
-    System.out.println(String.join(" ", "**     **", "**     **", "**     **", "***.     "));
-    System.out.println(String.join(" ", "**     **", "**     **", "*******  ", "    ^**. "));
-    System.out.println(String.join(" ", " *     * ", " *     * ", "**       ", "**     **"));
-    System.out.println(String.join(" ", "   ***   ", "   ***   ", "**       ", "  *****  "));
+    String[] lines = new String[6];
+    lines[0] = String.join(" ", "  .***.  ", "  .***.  ", "*******. ", " .*****. ");
+    lines[1] = String.join(" ", " *     * ", " *     * ", "**     **", "**     **");
+    lines[2] = String.join(" ", "**     **", "**     **", "**     **", "***.     ");
+    lines[3] = String.join(" ", "**     **", "**     **", "*******  ", "    ^**. ");
+    lines[4] = String.join(" ", " *     * ", " *     * ", "**       ", "**     **");
+    lines[5] = String.join(" ", "   ***   ", "   ***   ", "**       ", "  *****  ");
+
+    for (String line: lines) {
+      System.out.println(line);
+    }
   }
 }
